319a12085a16168b35778d5fdf2ea708f74c34e0
rzakadragon <rzakadragon@gmail.com>
2017-01-30 01:52:42 +0300

    slide 3 added, shadows added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+'use strict'
 
+jQuery(document).ready(function ($) {
+    $('.secondary').hide()
+    $('.show-all').hide()
+    $('.btn-more').show()
+
+    $('.place-group .btn-more').on('click', function () {
+        /** show .secondary places and 'show-all' btn */
+        var container = $(this).parents('.place-group')
+        container.find('.secondary').show()
+        container.find('.show-all').show()
+
+        /** hide the button */
+        $(this).hide()
+    })
+})
+
+var arr
